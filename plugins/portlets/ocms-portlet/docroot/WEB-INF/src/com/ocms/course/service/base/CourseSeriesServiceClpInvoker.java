963e7d7ae37d945d124ff3feb33493e31967bbf0
Vijay Chandra <vijay.c@madronesoft.com>
2015-01-16 16:32:21 +0530

    Missed files for contact

--- plugins/portlets/ocms-portlet/docroot/WEB-INF/src/com/ocms/course/service/base/CourseSeriesServiceClpInvoker.java
+++ plugins/portlets/ocms-portlet/docroot/WEB-INF/src/com/ocms/course/service/base/CourseSeriesServiceClpInvoker.java
@@ -24,24 +24,24 @@ import java.util.Arrays;
  */
 public class CourseSeriesServiceClpInvoker {
 	public CourseSeriesServiceClpInvoker() {
-		_methodName30 = "getBeanIdentifier";
+		_methodName36 = "getBeanIdentifier";
 
-		_methodParameterTypes30 = new String[] {  };
+		_methodParameterTypes36 = new String[] {  };
 
-		_methodName31 = "setBeanIdentifier";
+		_methodName37 = "setBeanIdentifier";
 
-		_methodParameterTypes31 = new String[] { "java.lang.String" };
+		_methodParameterTypes37 = new String[] { "java.lang.String" };
 	}
 
 	public Object invokeMethod(String name, String[] parameterTypes,
 		Object[] arguments) throws Throwable {
-		if (_methodName30.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes30, parameterTypes)) {
+		if (_methodName36.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes36, parameterTypes)) {
 			return CourseSeriesServiceUtil.getBeanIdentifier();
 		}
 
-		if (_methodName31.equals(name) &&
-				Arrays.deepEquals(_methodParameterTypes31, parameterTypes)) {
+		if (_methodName37.equals(name) &&
+				Arrays.deepEquals(_methodParameterTypes37, parameterTypes)) {
 			CourseSeriesServiceUtil.setBeanIdentifier((java.lang.String)arguments[0]);
 
 			return null;
@@ -50,8 +50,8 @@ public class CourseSeriesServiceClpInvoker {
 		throw new UnsupportedOperationException();
 	}
 
-	private String _methodName30;
-	private String[] _methodParameterTypes30;
-	private String _methodName31;
-	private String[] _methodParameterTypes31;
+	private String _methodName36;
+	private String[] _methodParameterTypes36;
+	private String _methodName37;
+	private String[] _methodParameterTypes37;
 }
