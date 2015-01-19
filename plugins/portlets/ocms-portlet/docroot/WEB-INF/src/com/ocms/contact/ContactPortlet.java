7face045c4ca63cfb4564520ada4d07b16dc590c
Vijay Chandra <vijay.c@madronesoft.com>
2015-01-19 15:35:48 +0530

    UI and portlet level files for contact

---
+++ plugins/portlets/ocms-portlet/docroot/WEB-INF/src/com/ocms/contact/ContactPortlet.java
@@ -0,0 +1,11 @@
+package com.ocms.contact;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class ContactPortlet
+ */
+public class ContactPortlet extends MVCPortlet {
+ 
+
+}
