263ecc6a83e949a1aaf858852c7268b8d7c78f0e
Vijay Chandra <vijay.c@madronesoft.com>
2015-01-19 11:07:12 +0530

    More missed files

---
+++ plugins/portlets/ocms-portlet/docroot/WEB-INF/service/com/ocms/course/service/ContactServiceWrapper.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.ocms.course.service;
+
+import com.liferay.portal.service.ServiceWrapper;
+
+/**
+ * Provides a wrapper for {@link ContactService}.
+ *
+ * @author doraraj
+ * @see ContactService
+ * @generated
+ */
+public class ContactServiceWrapper implements ContactService,
+	ServiceWrapper<ContactService> {
+	public ContactServiceWrapper(ContactService contactService) {
+		_contactService = contactService;
+	}
+
+	/**
+	* Returns the Spring bean ID for this bean.
+	*
+	* @return the Spring bean ID for this bean
+	*/
+	@Override
+	public java.lang.String getBeanIdentifier() {
+		return _contactService.getBeanIdentifier();
+	}
+
+	/**
+	* Sets the Spring bean ID for this bean.
+	*
+	* @param beanIdentifier the Spring bean ID for this bean
+	*/
+	@Override
+	public void setBeanIdentifier(java.lang.String beanIdentifier) {
+		_contactService.setBeanIdentifier(beanIdentifier);
+	}
+
+	@Override
+	public java.lang.Object invokeMethod(java.lang.String name,
+		java.lang.String[] parameterTypes, java.lang.Object[] arguments)
+		throws java.lang.Throwable {
+		return _contactService.invokeMethod(name, parameterTypes, arguments);
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #getWrappedService}
+	 */
+	public ContactService getWrappedContactService() {
+		return _contactService;
+	}
+
+	/**
+	 * @deprecated As of 6.1.0, replaced by {@link #setWrappedService}
+	 */
+	public void setWrappedContactService(ContactService contactService) {
+		_contactService = contactService;
+	}
+
+	@Override
+	public ContactService getWrappedService() {
+		return _contactService;
+	}
+
+	@Override
+	public void setWrappedService(ContactService contactService) {
+		_contactService = contactService;
+	}
+
+	private ContactService _contactService;
+}
