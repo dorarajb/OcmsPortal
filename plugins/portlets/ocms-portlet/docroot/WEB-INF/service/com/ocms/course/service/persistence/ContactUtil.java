139cbfd6a0a30df217eae226fc3852837a532802
Vijay Chandra <vijay.c@madronesoft.com>
2015-01-19 11:08:37 +0530

    Missed files

---
+++ plugins/portlets/ocms-portlet/docroot/WEB-INF/service/com/ocms/course/service/persistence/ContactUtil.java
@@ -0,0 +1,422 @@
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.ocms.course.service.persistence;
+
+import com.liferay.portal.kernel.bean.PortletBeanLocatorUtil;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ReferenceRegistry;
+import com.liferay.portal.service.ServiceContext;
+
+import com.ocms.course.model.Contact;
+
+import java.util.List;
+
+/**
+ * The persistence utility for the contact service. This utility wraps {@link ContactPersistenceImpl} and provides direct access to the database for CRUD operations. This utility should only be used by the service layer, as it must operate within a transaction. Never access this utility in a JSP, controller, model, or other front-end class.
+ *
+ * <p>
+ * Caching information and settings can be found in <code>portal.properties</code>
+ * </p>
+ *
+ * @author doraraj
+ * @see ContactPersistence
+ * @see ContactPersistenceImpl
+ * @generated
+ */
+public class ContactUtil {
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Modify <code>service.xml</code> and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#clearCache()
+	 */
+	public static void clearCache() {
+		getPersistence().clearCache();
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#clearCache(com.liferay.portal.model.BaseModel)
+	 */
+	public static void clearCache(Contact contact) {
+		getPersistence().clearCache(contact);
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#countWithDynamicQuery(DynamicQuery)
+	 */
+	public static long countWithDynamicQuery(DynamicQuery dynamicQuery)
+		throws SystemException {
+		return getPersistence().countWithDynamicQuery(dynamicQuery);
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#findWithDynamicQuery(DynamicQuery)
+	 */
+	public static List<Contact> findWithDynamicQuery(DynamicQuery dynamicQuery)
+		throws SystemException {
+		return getPersistence().findWithDynamicQuery(dynamicQuery);
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#findWithDynamicQuery(DynamicQuery, int, int)
+	 */
+	public static List<Contact> findWithDynamicQuery(
+		DynamicQuery dynamicQuery, int start, int end)
+		throws SystemException {
+		return getPersistence().findWithDynamicQuery(dynamicQuery, start, end);
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#findWithDynamicQuery(DynamicQuery, int, int, OrderByComparator)
+	 */
+	public static List<Contact> findWithDynamicQuery(
+		DynamicQuery dynamicQuery, int start, int end,
+		OrderByComparator orderByComparator) throws SystemException {
+		return getPersistence()
+				   .findWithDynamicQuery(dynamicQuery, start, end,
+			orderByComparator);
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#update(com.liferay.portal.model.BaseModel)
+	 */
+	public static Contact update(Contact contact) throws SystemException {
+		return getPersistence().update(contact);
+	}
+
+	/**
+	 * @see com.liferay.portal.service.persistence.BasePersistence#update(com.liferay.portal.model.BaseModel, ServiceContext)
+	 */
+	public static Contact update(Contact contact, ServiceContext serviceContext)
+		throws SystemException {
+		return getPersistence().update(contact, serviceContext);
+	}
+
+	/**
+	* Returns all the contacts where contactId = &#63;.
+	*
+	* @param contactId the contact ID
+	* @return the matching contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.ocms.course.model.Contact> findBycontactId(
+		long contactId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().findBycontactId(contactId);
+	}
+
+	/**
+	* Returns a range of all the contacts where contactId = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.ocms.course.model.impl.ContactModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param contactId the contact ID
+	* @param start the lower bound of the range of contacts
+	* @param end the upper bound of the range of contacts (not inclusive)
+	* @return the range of matching contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.ocms.course.model.Contact> findBycontactId(
+		long contactId, int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().findBycontactId(contactId, start, end);
+	}
+
+	/**
+	* Returns an ordered range of all the contacts where contactId = &#63;.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.ocms.course.model.impl.ContactModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param contactId the contact ID
+	* @param start the lower bound of the range of contacts
+	* @param end the upper bound of the range of contacts (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of matching contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.ocms.course.model.Contact> findBycontactId(
+		long contactId, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence()
+				   .findBycontactId(contactId, start, end, orderByComparator);
+	}
+
+	/**
+	* Returns the first contact in the ordered set where contactId = &#63;.
+	*
+	* @param contactId the contact ID
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the first matching contact
+	* @throws com.ocms.course.NoSuchContactException if a matching contact could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact findBycontactId_First(
+		long contactId,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException,
+			com.ocms.course.NoSuchContactException {
+		return getPersistence()
+				   .findBycontactId_First(contactId, orderByComparator);
+	}
+
+	/**
+	* Returns the first contact in the ordered set where contactId = &#63;.
+	*
+	* @param contactId the contact ID
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the first matching contact, or <code>null</code> if a matching contact could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact fetchBycontactId_First(
+		long contactId,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence()
+				   .fetchBycontactId_First(contactId, orderByComparator);
+	}
+
+	/**
+	* Returns the last contact in the ordered set where contactId = &#63;.
+	*
+	* @param contactId the contact ID
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the last matching contact
+	* @throws com.ocms.course.NoSuchContactException if a matching contact could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact findBycontactId_Last(
+		long contactId,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException,
+			com.ocms.course.NoSuchContactException {
+		return getPersistence()
+				   .findBycontactId_Last(contactId, orderByComparator);
+	}
+
+	/**
+	* Returns the last contact in the ordered set where contactId = &#63;.
+	*
+	* @param contactId the contact ID
+	* @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	* @return the last matching contact, or <code>null</code> if a matching contact could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact fetchBycontactId_Last(
+		long contactId,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence()
+				   .fetchBycontactId_Last(contactId, orderByComparator);
+	}
+
+	/**
+	* Removes all the contacts where contactId = &#63; from the database.
+	*
+	* @param contactId the contact ID
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void removeBycontactId(long contactId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		getPersistence().removeBycontactId(contactId);
+	}
+
+	/**
+	* Returns the number of contacts where contactId = &#63;.
+	*
+	* @param contactId the contact ID
+	* @return the number of matching contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static int countBycontactId(long contactId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().countBycontactId(contactId);
+	}
+
+	/**
+	* Caches the contact in the entity cache if it is enabled.
+	*
+	* @param contact the contact
+	*/
+	public static void cacheResult(com.ocms.course.model.Contact contact) {
+		getPersistence().cacheResult(contact);
+	}
+
+	/**
+	* Caches the contacts in the entity cache if it is enabled.
+	*
+	* @param contacts the contacts
+	*/
+	public static void cacheResult(
+		java.util.List<com.ocms.course.model.Contact> contacts) {
+		getPersistence().cacheResult(contacts);
+	}
+
+	/**
+	* Creates a new contact with the primary key. Does not add the contact to the database.
+	*
+	* @param contactId the primary key for the new contact
+	* @return the new contact
+	*/
+	public static com.ocms.course.model.Contact create(long contactId) {
+		return getPersistence().create(contactId);
+	}
+
+	/**
+	* Removes the contact with the primary key from the database. Also notifies the appropriate model listeners.
+	*
+	* @param contactId the primary key of the contact
+	* @return the contact that was removed
+	* @throws com.ocms.course.NoSuchContactException if a contact with the primary key could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact remove(long contactId)
+		throws com.liferay.portal.kernel.exception.SystemException,
+			com.ocms.course.NoSuchContactException {
+		return getPersistence().remove(contactId);
+	}
+
+	public static com.ocms.course.model.Contact updateImpl(
+		com.ocms.course.model.Contact contact)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().updateImpl(contact);
+	}
+
+	/**
+	* Returns the contact with the primary key or throws a {@link com.ocms.course.NoSuchContactException} if it could not be found.
+	*
+	* @param contactId the primary key of the contact
+	* @return the contact
+	* @throws com.ocms.course.NoSuchContactException if a contact with the primary key could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact findByPrimaryKey(long contactId)
+		throws com.liferay.portal.kernel.exception.SystemException,
+			com.ocms.course.NoSuchContactException {
+		return getPersistence().findByPrimaryKey(contactId);
+	}
+
+	/**
+	* Returns the contact with the primary key or returns <code>null</code> if it could not be found.
+	*
+	* @param contactId the primary key of the contact
+	* @return the contact, or <code>null</code> if a contact with the primary key could not be found
+	* @throws SystemException if a system exception occurred
+	*/
+	public static com.ocms.course.model.Contact fetchByPrimaryKey(
+		long contactId)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().fetchByPrimaryKey(contactId);
+	}
+
+	/**
+	* Returns all the contacts.
+	*
+	* @return the contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.ocms.course.model.Contact> findAll()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().findAll();
+	}
+
+	/**
+	* Returns a range of all the contacts.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.ocms.course.model.impl.ContactModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of contacts
+	* @param end the upper bound of the range of contacts (not inclusive)
+	* @return the range of contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.ocms.course.model.Contact> findAll(
+		int start, int end)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().findAll(start, end);
+	}
+
+	/**
+	* Returns an ordered range of all the contacts.
+	*
+	* <p>
+	* Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS} will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent and pagination is required (<code>start</code> and <code>end</code> are not {@link com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS}), then the query will include the default ORDER BY logic from {@link com.ocms.course.model.impl.ContactModelImpl}. If both <code>orderByComparator</code> and pagination are absent, for performance reasons, the query will not have an ORDER BY clause and the returned result set will be sorted on by the primary key in an ascending order.
+	* </p>
+	*
+	* @param start the lower bound of the range of contacts
+	* @param end the upper bound of the range of contacts (not inclusive)
+	* @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	* @return the ordered range of contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static java.util.List<com.ocms.course.model.Contact> findAll(
+		int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator orderByComparator)
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().findAll(start, end, orderByComparator);
+	}
+
+	/**
+	* Removes all the contacts from the database.
+	*
+	* @throws SystemException if a system exception occurred
+	*/
+	public static void removeAll()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		getPersistence().removeAll();
+	}
+
+	/**
+	* Returns the number of contacts.
+	*
+	* @return the number of contacts
+	* @throws SystemException if a system exception occurred
+	*/
+	public static int countAll()
+		throws com.liferay.portal.kernel.exception.SystemException {
+		return getPersistence().countAll();
+	}
+
+	public static ContactPersistence getPersistence() {
+		if (_persistence == null) {
+			_persistence = (ContactPersistence)PortletBeanLocatorUtil.locate(com.ocms.course.service.ClpSerializer.getServletContextName(),
+					ContactPersistence.class.getName());
+
+			ReferenceRegistry.registerReference(ContactUtil.class,
+				"_persistence");
+		}
+
+		return _persistence;
+	}
+
+	/**
+	 * @deprecated As of 6.2.0
+	 */
+	public void setPersistence(ContactPersistence persistence) {
+	}
+
+	private static ContactPersistence _persistence;
+}
